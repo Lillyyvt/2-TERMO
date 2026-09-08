3fd9cd15c86e878e19af040181a42e8c63bc0474
Livia Vitória Santana de Souza <lv212474@gmail.com>
2026-09-08 10:31:56 -0300

    08/09/2026

--- BACKEND/AULA_TARDE/AULA10/ex1.js
+++
@@ -1,3 +0,0 @@
-const moradores = ["Lilly", "Gustavo", "Juan"];
-
-console.log(moradores[1]); 
--- BACKEND/AULA_TARDE/AULA10/ex2.js
+++
@@ -1,3 +0,0 @@
-const alunos = ["Lucy", "Schroeder", "Snoopy"];
-console.log(`O primeiro aluno da lista é: ${alunos[0]}`);
-console.log(`Quantidade de alunos: ${alunos.length}`);
