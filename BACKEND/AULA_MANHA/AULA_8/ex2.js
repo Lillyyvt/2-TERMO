33dd72fc12931fdf56035b93a97abd5552f4d85f
Lillyyvt <lv212474@gmail.com>
2026-09-09 13:11:50 -0300

    09/09/2026

--- BACKEND/AULA_MANHA/AULA_8/ex2.js
+++
@@ -1,5 +0,0 @@
-for(let i = 0; i <= 99999; i += 10) {
-    console.log(`Contando... ${i}`);
-}
-
-console.log("Chegamos a 99999!");
--- BACKEND/AULA_MANHA/AULA_8/ex3.js
+++
@@ -1,5 +0,0 @@
-for(let i = 9; i >= 1 ; i -= 1) {
-    console.log(`Contando... ${i}`);
-}
-
-console.log("Chegamos ao fim!");
